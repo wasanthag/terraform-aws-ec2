cf35e98a49d2af52f45a0dec6e3a77e24dfe2660
Wasantha Gamage <wasanthag1302@gmail.com>
2020-03-30 09:28:26 -0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -5,14 +5,11 @@ variable "region" {
 variable "ami_id" {
   description = "ami image id"
   default     = "ami-03bac73ab0003d2ea"
-
 }
-
 variable "inst_type" {
   description = "instance type"
   default     = "t2.small"
 }
-
 variable "key_pair" {
   description = "key pair name"
 }
