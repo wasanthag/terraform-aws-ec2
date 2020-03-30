53f4bdf357bbcd02067f52ca69e43bf6746e9b68
Wasantha Gamage <wasanthag1302@gmail.com>
2020-03-30 09:27:40 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,9 +2,5 @@ resource "aws_instance" "exam" {
   ami           = var.ami_id
   instance_type = var.inst_type
   key_name      = var.key_pair
-  region = var.region
-
-  tags = {
-    version = "v1.0.2"
-  }
+  region       = var.region
 }
